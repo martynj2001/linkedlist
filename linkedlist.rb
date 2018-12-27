76a3e87026f744434ee3ca97a58c14e78b2928bc
Martyn Jones <martynj2010@icloud.com>
2018-12-27 19:59:14 +0000

    Initial commit
    Created LinkedList class Node

---
+++ linkedlist.rb
@@ -0,0 +1,49 @@
+#Tye OdinProject, Advanced Ruby - Data Structures assignment. Linked list Class
+
+class LinkedList
+
+	def append node
+	
+	end
+	
+	def prepend node
+	
+	end
+	
+	def size
+	
+	end
+	
+	def head
+	
+	end
+	
+	def tail
+	
+	end
+	
+	def at index
+	
+	end
+	
+	def pop
+	
+	end
+	
+	def contains?
+	
+	end
+	
+	def find data
+	
+	end
+	
+	
+
+end
+
+class Node
+
+	attr_accessor data, :next_node
+
+end
